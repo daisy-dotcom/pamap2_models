dd34671fa233a00b882be869766b1ad3d681994d
daisy-dotcom <61582926+daisy-dotcom@users.noreply.github.com>
2024-10-05 19:28:24 +0800

    moved code into try block

--- main.py
+++ main.py
@@ -28,26 +28,27 @@ def main():
 
     try:
         model = load_model(args.model)
+
+        data = pd.read_csv(args.input_file, header=None)
+        acc_data = data.filter(items=[1, 2, 3], axis=1)
+        eng_windows = rf_windowing(acc_data.to_numpy(dtype=np.float32))
+
+        labels = None
+        if model is not None:
+            labels = model.predict(eng_windows)
+
+        predictions = []
+        for label in labels:
+            label_name = pamap2_updated_labels[label]
+            predictions.append([label_name] * window_size)
+
+        predictions = np.array(predictions).reshape(-1)
+        result = data.join(pd.DataFrame(predictions), how='outer', lsuffix='_caller', rsuffix='_other')
+        result = result.dropna()
+        result.to_csv(args.output_file)
+
     except OSError:
-        print("Couldn't load the model")
-
-    data = pd.read_csv(args.input_file, header=None)
-    acc_data = data.filter(items=[1, 2, 3], axis=1)
-    eng_windows = rf_windowing(acc_data.to_numpy(dtype=np.float32))
-
-    labels = None
-    if model is not None:
-        labels = model.predict(eng_windows)
-
-    predictions = []
-    for label in labels:
-        label_name = pamap2_updated_labels[label]
-        predictions.append([label_name]*window_size)
-
-    predictions = np.array(predictions).reshape(-1)
-    result = data.join(pd.DataFrame(predictions), how='outer',lsuffix='_caller', rsuffix='_other')
-    result = result.dropna()
-    result.to_csv(args.output_file)
+        print("Couldn't load the model/files")
 
     #print('All Done')
     #print(data)
